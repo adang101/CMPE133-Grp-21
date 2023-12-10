b5b8a42f9cdad6824f85cb96b9765acd4e302d65
Alvin Dang <alvin.dang@yahoo.com>
2023-12-09 18:00:36 -0800

    Update run.py

--- Casso/run.py
+++ Casso/run.py
@@ -1,10 +1,10 @@
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'app'))
 from flask import render_template
 from flask_login import LoginManager
 from app import create_app
 from app.models import User
 import os
-import sys
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'app'))
 
 app = create_app()
 
