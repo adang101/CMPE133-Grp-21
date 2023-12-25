2fe765f7324be7e4b3a2a4aeed508f9d43131716
Alvin Dang <alvin.dang@yahoo.com>
2023-12-24 21:39:17 -0800

    static folder

--- Casso/app/__init__.py
+++ Casso/app/__init__.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 import os
 
 def create_app():
-    app = Flask("Casso", template_folder='templates')
+    app = Flask("Casso", template_folder='templates', static_folder='static')
     migrate = Migrate(app, db)
 
     # Database configuration (if you are using a database)
