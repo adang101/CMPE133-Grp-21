47ef9f1cb3caad203fac5b0a07052f3f0d412890
Alvin Dang <alvin.dang@yahoo.com>
2023-12-24 18:54:47 -0800

    Update run.py

--- Casso/run.py
+++ Casso/run.py
@@ -23,7 +23,7 @@ from app.models import User
 def index():
     # debugging
     print("index")
-    return render_template('/')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
