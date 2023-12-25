929613bf3c380f648bbc56b3beee9efa67ae224f
Alvin Dang <alvin.dang@yahoo.com>
2023-12-24 23:10:16 -0800

    absolute path for static dir

--- Casso/app/__init__.py
+++ Casso/app/__init__.py
@@ -5,8 +5,9 @@ import os
 
 def create_app():
     template_dir = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'templates')
+    static_dir = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'static')
     print("Template directory:", template_dir)
-    app = Flask("Casso", template_folder=template_dir, static_folder='static')
+    app = Flask("Casso", template_folder=template_dir, static_folder=static_dir)
     migrate = Migrate(app, db)
 
     print("Current working directory:", os.getcwd())
